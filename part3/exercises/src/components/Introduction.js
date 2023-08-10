a9eea73ef0afb6ec05d0630531b5136dd95c1fa8
keydamm <keyonamdamm@hotmail.com>
2023-08-10 16:34:36 -0500

    finished exercise

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+    return(
+        <div>
+            <h2>3 Fun Facts About this Hobby</h2>
+            <ol>
+                <li>Fully Customizable!</li>
+                <li>Lots of fun keycaps!</li>
+                <li>Expensive!</li>
+            </ol>
+        </div>
+    );
+}
